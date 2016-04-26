39525e579223c9b3553ff363c76ccd712104a5b7
putnamn21 <nathanputnam2@gmail.com>
2016-04-26 10:14:08 -0600

    added image switch

--- js/main.js
+++ js/main.js
@@ -17,3 +17,28 @@
         }); 
     
     });
+
+    var counter = 1;
+
+    function imgSources (){
+      
+      if(counter === 0){
+        counter = 1
+        return "./assets/AutomatedSmartLists.svg"
+      }else if (counter === 1){
+        counter = 2
+        return "./assets/MakeEmails.svg"
+      }else{
+        counter = 0
+        return "./assets/Stats.svg"
+      }
+    };
+
+
+    setTimeout(function(){
+      $('.drag-drop').remove();
+    }, 10000);
+    
+    setInterval(function(){
+      $('.main-graphic').children('.feature-graphic').attr('src', imgSources);
+    }, 10000)
